7c58d8d8e63e5b56304c5937a28d319659d3d14c
ahmadr127 <ahmdrifai808@gmail.com>
2026-03-24 16:52:14 +0700

    default password import

--- app/Http/Controllers/UserImportController.php
+++ app/Http/Controllers/UserImportController.php
@@ -176,6 +176,7 @@ class UserImportController extends Controller
                             'name' => $name,
                             'username' => $username,
                             'email' => $email,
+                            'password' => Hash::make('rsazra'),
                             'role_id' => $role->id,
                             'organization_unit_id' => $orgUnit ? $orgUnit->id : null,
                         ]);
