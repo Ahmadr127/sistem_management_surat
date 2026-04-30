0ea7f22ef7ccbd088618c05f65787a7115091292
ahmadr127 <ahmdrifai808@gmail.com>
2026-04-30 09:45:51 +0700

    feat: register app-layout component and add conditional HTTPS forcing in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use App\View\Components\AppLayout;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     {
         // Daftarkan component AppLayout
         Blade::component('app-layout', AppLayout::class);
+
+        // Force HTTPS jika di set di .env (untuk mengatasi warning form submission not secure di production)
+        if (env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
